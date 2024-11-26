4b24b8b145d328f20c36e184dedbc8d1f0e61881
tthp143 <tthipanet@gmail.com>
2024-11-26 11:13:15 +0700

    thanakon's testoop

---
+++ thanakon's oop/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Thanakon Thipanet";
+string student_id = "673450191-2";
+// string is Datatype -> ชนิดของตัวแปร
+// name is variable
+// "..." is value of variable
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is " + name);
+Console.WriteLine("My student id : " + student_id);
